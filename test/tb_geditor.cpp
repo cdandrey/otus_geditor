448ed3b87a56514ae5be28b781dcc32cbff29a86
Andrey <cdandrey@gmail.com>
2019-10-04 01:31:05 +0300

    Added tests

--- test/tb_geditor.cpp
+++ test/tb_geditor.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <sstream>
 
-TEST(test_geditor,add)
+TEST(test_geditor,add_and_remove)
 {
     std::ostringstream lout;    // local out
     auto backup_buf = std::cout.rdbuf();
@@ -12,9 +12,44 @@ TEST(test_geditor,add)
 
     Editor *e = new Editor();
     e->create();
-    e->add(Shape::Type::Line,Point(0,0),Point(9,9));
 
-    EXPECT_EQ(lout.str().substr(0,lout.str().find_first_of('\n',0)),"Create line: begin: {0,0} end: {9,9}");
+    e->add(Shape::Type::Line,Point(1,1),Point(1,10));
+    auto p0 = lout.str().find_first_of('\n',0);
+    EXPECT_EQ(lout.str().substr(0,p0),"Create line: begin: {1,1} end: {1,10}");
+    auto p1 = lout.str().find_first_of('\n',p0 + 1);
+    EXPECT_EQ(lout.str().substr(p0,p1),"Show line: begin: {1,1} end: {1,10}");
+
+    e->add(Shape::Type::Line,Point(1,1),Point(10,1));
+    p0 = p1;
+    p1 = lout.str().find_first_of('\n',p0 + 1);
+    EXPECT_EQ(lout.str().substr(p0,p1),"Create line: begin: {1,1} end: {10,1}");
+    p0 = p1;
+    p1 = lout.str().find_first_of('\n',p0 + 1);
+    EXPECT_EQ(lout.str().substr(p0,p1),"Show line: begin: {1,1} end: {10,1}");
+
+    e->add(Shape::Type::Line,Point(10,1),Point(10,10));
+    p0 = p1;
+    p1 = lout.str().find_first_of('\n',p0 + 1);
+    EXPECT_EQ(lout.str().substr(p0,p1),"Create line: begin: {10,1} end: {10,10}");
+    p0 = p1;
+    p1 = lout.str().find_first_of('\n',p0 + 1);
+    EXPECT_EQ(lout.str().substr(p0,p1),"Show line: begin: {10,1} end: {10,10}");
+
+    e->add(Shape::Type::Line,Point(1,10),Point(10,10));
+    p0 = p1;
+    p1 = lout.str().find_first_of('\n',p0 + 1);
+    EXPECT_EQ(lout.str().substr(p0,p1),"Create line: begin: {1,10} end: {10,10}");
+    p0 = p1;
+    p1 = lout.str().find_first_of('\n',p0 + 1);
+    EXPECT_EQ(lout.str().substr(p0,p1),"Show line: begin: {1,10} end: {10,10}");
+
+    e->add(Shape::Type::Circle,Point(5,5),4);
+    p0 = p1;
+    p1 = lout.str().find_first_of('\n',p0 + 1);
+    EXPECT_EQ(lout.str().substr(p0,p1),"Create circle: center: {5,5} radius: 4");
+    p0 = p1;
+    p1 = lout.str().find_first_of('\n',p0 + 1);
+    EXPECT_EQ(lout.str().substr(p0,p1),"Show circle: center: {5,5} radius: 4");
 
     std::cout.rdbuf(backup_buf);
 
