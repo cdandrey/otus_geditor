e3972ebe0180913b1a3246b32904e55efd660712
Andrey <cdandrey@gmail.com>
2019-10-04 01:38:23 +0300

    Fixed range substr

--- test/tb_geditor.cpp
+++ test/tb_geditor.cpp
@@ -16,39 +16,39 @@ TEST(test_geditor,add_and_remove)
     e->add(Shape::Type::Line,Point(1,1),Point(1,10));
     auto p0 = lout.str().find_first_of('\n',0);
     EXPECT_EQ(lout.str().substr(0,p0),"Create line: begin: {1,1} end: {1,10}");
-    auto p1 = lout.str().find_first_of('\n',p0 + 1);
+    auto p1 = lout.str().find_first_of('\n',++p0);
     EXPECT_EQ(lout.str().substr(p0,p1),"Show line: begin: {1,1} end: {1,10}");
 
     e->add(Shape::Type::Line,Point(1,1),Point(10,1));
-    p0 = p1;
-    p1 = lout.str().find_first_of('\n',p0 + 1);
+    p0 = p1 + 1;
+    p1 = lout.str().find_first_of('\n',p0);
     EXPECT_EQ(lout.str().substr(p0,p1),"Create line: begin: {1,1} end: {10,1}");
-    p0 = p1;
-    p1 = lout.str().find_first_of('\n',p0 + 1);
+    p0 = p1 + 1;
+    p1 = lout.str().find_first_of('\n',p0);
     EXPECT_EQ(lout.str().substr(p0,p1),"Show line: begin: {1,1} end: {10,1}");
 
     e->add(Shape::Type::Line,Point(10,1),Point(10,10));
-    p0 = p1;
-    p1 = lout.str().find_first_of('\n',p0 + 1);
+    p0 = p1 + 1;
+    p1 = lout.str().find_first_of('\n',p0);
     EXPECT_EQ(lout.str().substr(p0,p1),"Create line: begin: {10,1} end: {10,10}");
-    p0 = p1;
-    p1 = lout.str().find_first_of('\n',p0 + 1);
+    p0 = p1 + 1;
+    p1 = lout.str().find_first_of('\n',p0);
     EXPECT_EQ(lout.str().substr(p0,p1),"Show line: begin: {10,1} end: {10,10}");
 
     e->add(Shape::Type::Line,Point(1,10),Point(10,10));
-    p0 = p1;
-    p1 = lout.str().find_first_of('\n',p0 + 1);
+    p0 = p1 + 1;
+    p1 = lout.str().find_first_of('\n',p0);
     EXPECT_EQ(lout.str().substr(p0,p1),"Create line: begin: {1,10} end: {10,10}");
-    p0 = p1;
-    p1 = lout.str().find_first_of('\n',p0 + 1);
+    p0 = p1 + 1;
+    p1 = lout.str().find_first_of('\n',p0);
     EXPECT_EQ(lout.str().substr(p0,p1),"Show line: begin: {1,10} end: {10,10}");
 
     e->add(Shape::Type::Circle,Point(5,5),4);
-    p0 = p1;
-    p1 = lout.str().find_first_of('\n',p0 + 1);
+    p0 = p1 + 1;
+    p1 = lout.str().find_first_of('\n',p0);
     EXPECT_EQ(lout.str().substr(p0,p1),"Create circle: center: {5,5} radius: 4");
-    p0 = p1;
-    p1 = lout.str().find_first_of('\n',p0 + 1);
+    p0 = p1 + 1;
+    p1 = lout.str().find_first_of('\n',p0);
     EXPECT_EQ(lout.str().substr(p0,p1),"Show circle: center: {5,5} radius: 4");
 
     std::cout.rdbuf(backup_buf);
